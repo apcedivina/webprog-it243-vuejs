cbc874ec36c5a7a5a2979a25c76d7d6611cf3615
ElizDivz <ecdivina@student.apc.edu.ph>
2026-01-12 06:54:05 +0000

    appvue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,7 @@
-<script setup></script>
-
+App.vue
+ 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>yowww instant</h1>
 </template>
-
-<style scoped></style>
+ 
+<script></script>
