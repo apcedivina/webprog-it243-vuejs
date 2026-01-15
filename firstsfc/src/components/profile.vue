9a51db34e8fd9c769d68579aa4651d23b05479b7
ElizDivz <ecdivina@student.apc.edu.ph>
2026-01-15 15:13:45 +0800

    pppppppp

--- firstsfc/src/components/profile.vue
+++ firstsfc/src/components/profile.vue
@@ -1,39 +1,24 @@
 <!DOCTYPE html>
+<html>
 <head>
+<title>Page Title</title>
+</head>
 <body>
-    <header>
-        <h1>My Personal Web Page</h1>
-    </header>
  
-    <main class="container">
-        <section class="grid grid1">
-            <div><strong> Name:</strong> Elisha Shimei C. Divina</div>
-            <div><strong> Birthday:</strong> March 24, 2005</div>
-            <div><strong> Current Address:</strong> Fort Bonifacio, Taguig City</div>
-        </section>
+<h1>Personal Profile Web page </h1>
+<p>
  
-        <section class="grid grid2">
-            <div><strong> School:</strong> Asia Pacific College</div>
-            <div><strong> Course:</strong> BSIT</div>
-            <div><strong> Year & Section:</strong> 2nd Year IT243</div>
-        </section>
+About Me<br>
+Graduate of Upper Bicutan National High School With Honors<br>
+BSIT<br>
+Programming, Gaming, HTML<br>
+Online gaming, listening to music<br>
+To become a successful IT professional in the future<br>
+Picture Gallery<br>
+ <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhw8O7418E9ih9TFiiaXPilw-ndrqTiX2YKQ&s" alt="Girl with a jacket" width="500" height="600">
+</p>
  
-        <section class="grid grid3">
-            <div><strong> Hobbies:</strong> Music, Gaming, Collecting, Cooking</div>
-            <div><strong> Dislikes:</strong> Bugs, Seafood</div>
-            <div><strong> Favorite Colors:</strong> Pink, Beige, Red, Purple</div>
-        </section>
  
-        <section class="grid grid4">
-            <div><strong> Motto:</strong> "Let's do our best rather than being the best - Jeong Yunho"</div>
-            <div><strong> Life Goals:</strong> To become a successful IT professional in the future</div>
-        </section>
-        <section class="grid grid5">
-            <div><img src="https://media.timeout.com/images/106259867/image.webp"></div>
-            <div><img src="https://i.pinimg.com/736x/80/37/77/803777b4fced28d062d1d6553182711d.jpg"></div>
-            <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDBUa3FZfop_eR6MFOURfGDoc2f0TK3pURhg&s"></div>
-        </section>
-    </main>
-  <a href="html/resources.html">Resources</a>
+  
 </body>
 </html>
