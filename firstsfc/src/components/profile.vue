f2016517f2e26c357b41090e5e2b1caa38aa2404
ElizDivz <ecdivina@student.apc.edu.ph>
2026-01-15 15:09:03 +0800

    profil

---
+++ firstsfc/src/components/profile.vue
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Personal Web Page</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/flexbox.css">
+</head>
+<body>
+    <header>
+        <h1>My Personal Web Page</h1>
+    </header>
+ 
+    <main class="container">
+        <section class="grid grid1">
+            <div><strong> Name:</strong> Elisha Shimei C. Divina</div>
+            <div><strong> Birthday:</strong> March 24, 2005</div>
+            <div><strong> Current Address:</strong> Fort Bonifacio, Taguig City</div>
+        </section>
+ 
+        <section class="grid grid2">
+            <div><strong> School:</strong> Asia Pacific College</div>
+            <div><strong> Course:</strong> BSIT</div>
+            <div><strong> Year & Section:</strong> 2nd Year IT243</div>
+        </section>
+ 
+        <section class="grid grid3">
+            <div><strong> Hobbies:</strong> Music, Gaming, Collecting, Cooking</div>
+            <div><strong> Dislikes:</strong> Bugs, Seafood</div>
+            <div><strong> Favorite Colors:</strong> Pink, Beige, Red, Purple</div>
+        </section>
+ 
+        <section class="grid grid4">
+            <div><strong> Motto:</strong> "Let's do our best rather than being the best - Jeong Yunho"</div>
+            <div><strong> Life Goals:</strong> To become a successful IT professional in the future</div>
+        </section>
+        <section class="grid grid5">
+            <div><img src="https://media.timeout.com/images/106259867/image.webp"></div>
+            <div><img src="https://i.pinimg.com/736x/80/37/77/803777b4fced28d062d1d6553182711d.jpg"></div>
+            <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDBUa3FZfop_eR6MFOURfGDoc2f0TK3pURhg&s"></div>
+        </section>
+    </main>
+  <a href="html/resources.html">Resources</a>
+</body>
+</html>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,8 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
+import profile from './components/profile.vue'
  
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
+app.component('profile', profile)
 app.mount('#app') 
