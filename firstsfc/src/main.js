9d7826b1033882f9b9572c10b8600b8b75b15035
ElizDivz <ecdivina@student.apc.edu.ph>
2026-01-26 15:24:48 +0800

    vuev

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
- 
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import profile from './components/profile.vue'
- 
+import Country from './components/Country.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
+
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
-app.component('profile', profile)
-app.mount('#app') 
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
+app.mount('#app')
